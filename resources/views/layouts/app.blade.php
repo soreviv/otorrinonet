63535336257c1035654e930f08e3a334b805cdb3
Alejandro Viveros Dominguez <99574426+soreviv@users.noreply.github.com>
2025-11-27 04:40:22 +0000

    feat: Add dark mode toggle to layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
     </head>
     <body class="antialiased">
         <div class="container">
